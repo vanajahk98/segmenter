cec7e4644ab5431cb69ef8ffa5406e34d6f5fa2b
lune <lune.manoj@gmail.com>
2016-02-23 11:06:23 +0530

    Added wiki Q score, fixed a few things, boosted qscore contributions 5x

--- segmenter.py
+++ segmenter.py
@@ -13,7 +13,7 @@ import re
 import numpy
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 stdout_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stdout_handler)
 
@@ -22,7 +22,19 @@ U = 5 # Max length of a segment
 E = 5 # Max sub segments of a segment to be considered
 REGEX = re.compile('[%s]' % re.escape(punctuation))
 
+WIKI_Q_FILE_PATH = 'WikiQsEng/WikiQsEng.txt'
 
+def load_wiki_q():
+    d = {}
+    with open(WIKI_Q_FILE_PATH) as f:
+        for line in f:
+            kwd, p = line.strip().rsplit(',', 1)
+            kwd = kwd.replace('_', ' ')
+            kwd = REGEX.sub(' ', kwd)
+            d[kwd] = max(float(p), float(0))
+    return d
+
+WIKI_Q = load_wiki_q()
 
 def find_ngrams(in_toks, n):
       return zip(*[in_toks[i:] for i in range(n)])
@@ -35,8 +47,9 @@ def find_all_grams(in_toks):
 
 def split_to_words(in_str):
     in_str = in_str.lower()
-    in_str = in_str.replace('.', ' ')
-    in_str = REGEX.sub('', in_str)
+    in_str = in_str.replace('\'', '')
+    in_str = in_str.replace('`', '')
+    in_str = REGEX.sub(' ', in_str)
     tokens = filter(None, [each.strip() for each in in_str.split(' ')])
     return tokens
 
@@ -44,27 +57,49 @@ def sticky_score(segment, cache={}):
     """
     Calculate the stickiness score of a segment.
     """
-    if len(segment) <= 1:
-        score = 1
-    else:
-        scp = get_scp(segment, cache)
-        score = 2/(1 + numpy.exp(-1 * scp))
+    scp = get_scp(segment, cache)
+    l_normal_factor = normalize_seg_length(len(segment))
+    q_score = wiki_keyphraseness(segment)
+    score = 2/(1 + numpy.exp(-1 * scp)) \
+            * numpy.exp(10*q_score) \
+            * l_normal_factor
+    print ' '.join(segment), "SCP: %s, l_score: %s, q_score: %s, total %s" % (scp, l_normal_factor, q_score, score)
     return score
 
+def wiki_keyphraseness(segment):
+    '''
+    Q(s) Length Normalization
+    '''
+    q_score = WIKI_Q.get(' '.join(segment), 0)
+    return float(q_score)
+   
+def normalize_seg_length(seg_length):
+    '''
+    L(s) Length Normalization
+    '''
+    factor = 1
+    if seg_length > 1:
+        factor = float(seg_length - 1)/seg_length
+    return factor
+
 def get_scp(segment, cache):
-    v1 = numpy.array([cache[i] for i in segment[:-1]])
-    v2 = numpy.array([cache[i] for i in segment[1:]])
+    '''
+    Global P(s)
+    '''
     n = len(segment)
-    segment_str = ' '.join([each.strip() for each in segment])
-    score = cache.get(segment_str)
-    scp = ((n - 1) * numpy.exp(2*score))/sum(numpy.exp(v1 + v2))
+    if n == 1:
+        scp = 2*cache.get(' '.join(segment))
+    else:
+        v = [cache[' '.join(segment[:i])] + cache[' '.join(segment[i:])] for i in range(1, n)]
+        segment_str = ' '.join([each.strip() for each in segment])
+        score = cache.get(segment_str)
+        scp = 2*score - (sum(numpy.exp(v))/(n - 1))
     return scp
 
 def preprocess(toks):
     all_grams = list(set(find_all_grams(toks)))
     lm = LangModel('body')
     pvals = lm.get_jp(all_grams)
-    pprint(pvals)
     return pvals
 
 def segment(in_str):
@@ -84,14 +119,14 @@ def segment(in_str):
             S[i].append(([current_segment], current_score))
             logger.debug(u"Current Segment: Start: %s, len: %s, Score: %0.5f, %s", \
                     i, len(current_segment), current_score, current_segment)
-            continue # ?????
+            #continue # ?????
         # Do sub-segments
         for j in xrange(i):
-            if i - j <= U:
+            if i - j < U:
                 seg_1 = current_segment[:j+1]
                 seg_2 = current_segment[j+1:]
                 score_2 = sticky_score(seg_2, cache)
-                logger.debug(u"Split: (%s, %s, %s), %s ", 0, j, i, current_segment)
+                logger.debug(u"Consider: (%s, %s), %s ", j, i, current_segment[j:])
                 logger.debug(u"Considered Segment: Start: %s, end: %s, Score: %0.5f, %s", j+1, i, score_2, current_segment[j+1:])
                 # update segment
                 new_segs = [(seg + [seg_2], seg_score + score_2) for (seg, seg_score) in S[j]]
@@ -106,10 +141,16 @@ def segment(in_str):
 
 
 if __name__ == '__main__':
-    T = u'We have some delightful new food in the cafeteria. Awesome!!!'
-    T = u'''Be really careful with those early entrance polls. Not intended for making projections. Weren't accurate in Iowa. '''
-    T = u'this youth olympic games sailing competition'
+    T = u'''1st sentence: "Antonin Scalia...devoted his professional life to making the [US] a less fair, less tolerant, and less admirable democracy"'''
+    T = u'youth olympic games sailing competition'
+    T = u'''French leading daily Le Monde said in editorial, "the horizon of Indian democracy has been oddly clouded since the coming to power of Modi." '''
+    '''
     k = segment(T)
     print T
     print "Best Segmentation : "
     print '[', ' ]['.join([' '.join(each) for each in k]), ']'
+    '''
+    cache = preprocess(split_to_words(T))
+    for each in find_all_grams(split_to_words(T)):
+        each = each.split()
+        print  each, numpy.exp(get_scp(each, cache)), numpy.exp(cache[' '.join(each)])
